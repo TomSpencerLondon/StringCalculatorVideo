fb6862567f177f025fc23de8f9df9b9e4cb1a761
Tom <thomas.spencer@codurance.com>
2020-08-02 22:16:44 +0100

    Add tests for multiple longer delimiters

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -5,17 +5,24 @@ import java.util.Arrays;
 public class StringCalculator {
 
   public int add(String input) {
-    if (input.length() == 0){
-      return 0;
+    StringBuilder regex = new StringBuilder("\\n,");
+    StringBuilder result = new StringBuilder();
+
+    if (input.startsWith("//")){
+      regex.append(input, input.indexOf("//"), input.indexOf("\n"));
+      result.append(input.substring(input.indexOf("\n")).trim());
+    }else {
+      result.append(input);
     }
 
-    return Arrays.stream(input.split("[,\n]"))
+    return Arrays.stream(result.toString().split("[" + regex + "]"))
         .map(String::strip)
+        .filter(i -> !i.isBlank())
         .mapToInt(Integer::parseInt)
+        .filter(n -> n <= 1000)
         .peek(n -> {
           if (n < 0) throw new IllegalArgumentException("Negatives not allowed");
-        })
-        .sum();
+        }).sum();
 
   }
 }
--- src/test/java/com/codurance/StringCalculatorShould.java
+++ src/test/java/com/codurance/StringCalculatorShould.java
@@ -35,7 +35,11 @@ public class StringCalculatorShould {
       "1, 0; 1",
       "1, 2; 3",
       "1, 2, 3, 4, 5, 6, 7, 8, 9; 45",
-      "'1\n2,3'; 6"
+      "'1\n2,3'; 6",
+      "'1001, 2'; 2",
+      "'//[***]\n1***2***3'; 6",
+      "'//[*][%]\n1*2%3'; 6",
+      "'//[foo][bar]\n1foo2bar3'; 6"
   }, delimiter = ';')
   void returns_correct_output_with_input(String input, int output) {
     assertEquals(output, stringCalculator.add(input));
